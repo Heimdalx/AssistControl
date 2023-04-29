1580f123bb032fe25b6841466796e8fc0556e9ce
David Vasquez Rios <davidvasquez235678@correo.itm.edu.co>
2023-04-28 23:20:30 -0500

    Seeder done

---
+++ AssistControl.API/Data/SeedDb.cs
@@ -0,0 +1,37 @@
+﻿using AssistControl.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Sales.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context= context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckTickets();
+        }
+        private async Task CheckTickets()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for(int i = 0; i <= 50000; i++)
+                {
+                    _context.Tickets.Add(new Ticket {Porteria = String.Empty});
+                    Console.WriteLine(i);
+
+                }
+                await _context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
+
